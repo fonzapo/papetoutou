e02868115602352b5ca622d29a155b150846d1f9
fonzapo <vincentboyer063@gmail.com>
2021-04-28 19:12:31 +0200

    base code ok

--- game/main.js
+++ game/main.js
@@ -1,22 +1,21 @@
 class Toutou {
     constructor(x, y, ctx) {
-        this._toutouX = x;
-        this._toutouY = y;
+        this._x = x;
+        this._y = y;
         this._ctx = ctx;
     }
     show() {
         const img = new Image();
+        img.onload = () => {
+            this._ctx.drawImage(img, this._x, this._y, 100, 80);
+        }
         img.src = '../assets/img/sprites/pape/pape.png';
-        this._ctx.drawImage(img, this._toutouX, this._toutouY, 100, 80);
-        /*ctx.fillStyle = '#000000';
-        ctx.fillRect(0, 0, 100, 100)*/
     }
 }
 
 let toutou;
 
 const canvas = document.querySelector('#canvas');
-//ctx sert à définir le contexte (2d ou 3d);
 const ctx = canvas.getContext('2d');
 
 const c_width = 800;
@@ -26,10 +25,11 @@ canvas.width = c_width;
 canvas.height = c_height;
 
 const setup = () => {
-    toutou = new Toutou(100, 500, ctx);
+    toutou = new Toutou(100, 200, ctx);
 };
 
 const draw = () => {
+    ctx.clearRect(0, 0, c_width, c_height);
     ctx.fillStyle = '#ffffff';
     ctx.fillRect(0, 0, c_width, c_height);
     toutou.show();
